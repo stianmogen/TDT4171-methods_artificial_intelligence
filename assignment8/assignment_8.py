c749fc42aaef1a27efdf4a91ddb52fdcfce778f7
Stian Mogen <stianfmo@stud.ntnu.no>
2023-03-10 09:27:56 +0100

    finished but should probably use sequential

--- assignment8/assignment_8.py
+++ assignment8/assignment_8.py
@@ -2,16 +2,13 @@ import pickle
 from typing import Dict, List, Any, Union
 import numpy as np
 import pandas as pd
-# Keras
 from keras import Input
-from keras.layers import Bidirectional, LSTM, Embedding, Dense, Activation, Lambda, RepeatVector, Permute, Concatenate, \
-    Flatten
-from tensorflow import keras
-import tensorflow as tf
+from keras.layers import Bidirectional, LSTM, Embedding, Dense, Flatten
 from keras.utils import pad_sequences
+# from keras_preprocessing.sequence import pad_sequences
 from keras.models import Model
-from keras import backend
-
+import tensorflow as tf
+import os
 
 
 def load_data() -> Dict[str, Union[List[Any], int]]:
@@ -34,9 +31,13 @@ def preprocess_data(data: Dict[str, Union[List[Any], int]]) -> Dict[str, Union[L
 
     return data
 
-def ff_model(vocab_size=1000):
-    input_tensor = Input(shape=(None, 66))
-    x = Embedding(input_dim=vocab_size, output_dim=128)(input_tensor)
+def ff_model(vocab_size=1000, num_features=66):
+    input_tensor = Input(shape=(num_features, ))
+    x = Embedding(input_dim=vocab_size, output_dim=64)(input_tensor)
+    x = Dense(256, activation='relu')(x)
+    # without the flatten layer the model will only produce 0.7 accuracy
+    x = Flatten()(x)
+    x = Dense(128, activation='sigmoid')(x)
     y = Dense(1, activation='sigmoid')(x)
 
     model = Model(input_tensor, y)
@@ -44,9 +45,10 @@ def ff_model(vocab_size=1000):
     return model
 
 
-def lstm_model(vocab_size=1000):
-    hidden_size = 64
-    input_tensor = Input(shape=(None,))
+def lstm_model(vocab_size=1000, num_features=66):
+    # high value of hidden size just for fun, 64 is sufficient
+    hidden_size = 256
+    input_tensor = Input(shape=(num_features,))
     x = Embedding(input_dim=vocab_size, output_dim=128)(input_tensor)
     x = Bidirectional(LSTM(units=hidden_size))(x)
 
@@ -54,7 +56,9 @@ def lstm_model(vocab_size=1000):
     model = Model(input_tensor, y)
     return model
 
-def train_model(data: Dict[str, Union[List[Any], np.ndarray, int]], model_type="feedforward", batch_size=1024, epochs=10) -> float:
+
+
+def train_model(data: Dict[str, Union[List[Any], np.ndarray, int]], model_type="feedforward", batch_size=1024, epochs=10) -> pd:
     """
     Build a neural network of type model_type and train the model on the data.
     Evaluate the accuracy of the model on test data.
@@ -62,35 +66,31 @@ def train_model(data: Dict[str, Union[List[Any], np.ndarray, int]], model_type="
     :param data: The dataset dictionary to train neural network on
     :param model_type: The model to be trained, either "feedforward" for feedforward network
                         or "recurrent" for recurrent network
-    :return: The accuracy of the model on test data
+    :return: The model and accuracy of the model on test data
     """
-
-    # TODO build the model given model_type, train it on (data["x_train"], data["y_train"])
-    #  and evaluate its accuracy on (data["x_test"], data["y_test"]). Return the accuracy
     x_train = data["x_train"]
     y_train = data["y_train"]
     vocab_size = data["vocab_size"]
+    seq_length, num_features = x_train.shape[0], x_train.shape[1]
 
-    if (model_type=="reccurent"):
+    if model_type== "recurrent":
         print("Creating LSTM model:")
-        model = lstm_model(vocab_size)
-        model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=['accuracy'])
+        model = lstm_model(vocab_size, num_features)
     else:
         print("Creating FF model")
-        model = ff_model(vocab_size)
-        model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=['accuracy'])
-    print("Training..")
+        model = ff_model(vocab_size, num_features)
 
+    # There might be an argument for using a different optimizer like adam for the FF network, however for the
+    # sake of comparison both use rmsprop in this case
+    model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=['accuracy'])
 
-    model_checkpoints = keras.callbacks.ModelCheckpoint(f"model.h5", save_best_only=True)
-    print("x_train", x_train.shape)
-    print("y_train", y_train.shape)
-    print(batch_size)
+    print("Training", model_type, "...")
     history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
-    model.save()
+
+    #model.save()
     model_history = pd.DataFrame(history.history)
     model_history['epoch'] = history.epoch
-    return model_history
+    return model_history, model_history['accuracy'].iloc[-1]
 
 def main() -> None:
     print("1. Loading data...")
@@ -98,16 +98,38 @@ def main() -> None:
     print("2. Preprocessing data...")
     keras_data = preprocess_data(keras_data)
     print("3. Training feedforward neural network...")
-    #fnn_test_accuracy = train_model(keras_data, model_type="feedforward")
-    #print('Model: Feedforward NN.\n'
-    #      f'Test accuracy: {fnn_test_accuracy:.3f}')
+    fnn_model_history, fnn_test_accuracy = train_model(keras_data, model_type="feedforward")
+    print('Model: Feedforward NN.\n'
+          f'Test accuracy: {fnn_test_accuracy:.3f}')
     print("4. Training recurrent neural network...")
-    rnn_test_accuracy = train_model(keras_data, model_type="recurrent")
+    rnn_model_history, rnn_test_accuracy = train_model(keras_data, model_type="recurrent")
     print('Model: Recurrent NN.\n'
           f'Test accuracy: {rnn_test_accuracy:.3f}')
 
 
 
 if __name__ == '__main__':
+    # Running application with cuda if gpu is available
+    os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+
+    built = tf.test.is_built_with_cuda()
+    print("tf is built with CUDA? ", built)
+    sys_details = tf.sysconfig.get_build_info()
+    # Only show actual errors, not warning
+    # Note that the warnings are quite nice for debugging
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+    gpus = tf.config.list_physical_devices('GPU')
+
+    print("Num GPUs Available: ", len(gpus))
+
+    if gpus:
+        # Restrict TensorFlow to only use the first GPU
+        try:
+            tf.config.set_visible_devices(gpus[0], 'GPU')
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+        except RuntimeError as e:
+            # Visible devices must be set before GPUs have been initialized
+            print(e)
     main()
 
